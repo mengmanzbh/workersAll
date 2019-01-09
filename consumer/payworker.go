9c8c61e0c779044044571b6d3b3cd3ba5a039bc5
mengmanzbh <1361006944@qq.com>
2019-01-09 11:30:05 +0800

    OK

--- consumer/payworker.go
+++ consumer/payworker.go
@@ -79,96 +79,90 @@ func main() {
 
             /******************查询订单状态确定是否为2*****************/
                 orderidstatus := getorderStatus(orderid)
-                if orderidstatus == "2" {
-                    fmt.Print("订单状态不正确,应该传状态为2的，你传入的订单状态为:",orderidstatus)
+                 if orderidstatus == "2" {
+                     fmt.Print("订单状态2可以减币操作:",orderidstatus)
+                                /******************发送订单*****************/
+                                token := getAccess(code)//根据前端传来的code获取token
+                                lastprice :=  getLastprice()//请求接口获取最新价格
+                                /**************将价格转成浮点数**************/
+                                total, _ := strconv.ParseFloat(totalprice, 64)
+                                last, _ := strconv.ParseFloat(lastprice, 64)
+                                resultdata := total/last
+                                resultstr := fmt.Sprintf("%.4f",resultdata)
+                                resultNum, _:= strconv.ParseFloat(resultstr, 64)
+                                fmt.Println("resultNum:",resultNum)
+                                /**************将价格转成浮点数**************/
+                                //请求地址
+                                juheURL :="http://op.juhe.cn/trainTickets/pay"
 
-                    t := time.Duration(dot_count)
-                    time.Sleep(t * time.Second)
-                    log.Printf("Done")
-                    d.Ack(false)
-                    
-                    return
-                }
+                                //初始化参数
+                                param:=url.Values{}
+
+                                //配置请求参数,方法内部已处理urlencode问题,中文参数可以直接传参
+                                param.Set("dtype","json") //返回的格式，json或xml，默认json
+                                param.Set("key",APPKEY)
+                                param.Set("orderid",orderid) //订单号，提交订单时会返回
+
+                                /***************先减币成功才发送订单*****************/
+                                data := make(map[string]interface{})
+                                data["access"] = token
+                                data["mark"] = "TGV"
+                                data["num"] = resultNum
+                                data["order_no"] = GetRandomString(6)
+                                bytesData, err := json.Marshal(data)
+                                if err != nil {
+                                    fmt.Println(err.Error() )
+                                    return
+                                }
+                                reader := bytes.NewReader(bytesData)
+                                url := "http://47.52.47.73:8112/auth/auth/subNum"
+                                request, err := http.NewRequest("POST", url, reader)
+                                if err != nil {
+                                    fmt.Println(err.Error())
+                                    return
+                                }
+                                request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+                                client := http.Client{}
+                                resp, err := client.Do(request)
+                                if err != nil {
+                                    fmt.Println(err.Error())
+                                    return
+                                }
+                                respBytes, err := ioutil.ReadAll(resp.Body)
+                                if err != nil {
+                                    fmt.Println(err.Error())
+                                    return
+                                }
+                                //返回结果
+                                var netReturn map[string]interface{}
+                                json.Unmarshal(respBytes,&netReturn)
+                                if netReturn["isSuccess"]==true{
+                                    fmt.Printf("减币成功:\r\n%v",netReturn)
+                                    /*****************减币成功********************/
+                                    
+                                    /************发送请求给聚合************/
+                                    data,err:=Post(juheURL,param)
+                                    if err!=nil{
+                                        fmt.Errorf("请求聚合失败,错误信息:\r\n%v",err)
+
+                                    }else{
+                                        var netReturn map[string]interface{}
+                                        json.Unmarshal(data,&netReturn)
+                                        fmt.Printf("请求聚合返回结果:\r\n%v",netReturn)
+                                    }
+                                    /***********发送请求给聚合************/
+
+                                }else{
+                                    /*****************减币失败********************/
+                                 
+                                }
+                                /***************减币成功才发送订单*****************/             
+                               /******************发送订单*****************/
+                     
+                 }else{
+                     fmt.Print("订单状态不对:",orderidstatus)
+                 }
             /******************查询订单状态确定是否为2*****************/
-
-
-			/******************发送订单*****************/
-				token := getAccess(code)//根据前端传来的code获取token
-				lastprice :=  getLastprice()//请求接口获取最新价格
-				/**************将价格转成浮点数**************/
-				total, _ := strconv.ParseFloat(totalprice, 64)
-				last, _ := strconv.ParseFloat(lastprice, 64)
-				resultdata := total/last
-				resultstr := fmt.Sprintf("%.4f",resultdata)
-				resultNum, _:= strconv.ParseFloat(resultstr, 64)
-				fmt.Println("resultNum:",resultNum)
-				/**************将价格转成浮点数**************/
-				//请求地址
-				juheURL :="http://op.juhe.cn/trainTickets/pay"
-
-				//初始化参数
-				param:=url.Values{}
-
-				//配置请求参数,方法内部已处理urlencode问题,中文参数可以直接传参
-				param.Set("dtype","json") //返回的格式，json或xml，默认json
-				param.Set("key",APPKEY)
-				param.Set("orderid",orderid) //订单号，提交订单时会返回
-
-				/***************先减币成功才发送订单*****************/
-				data := make(map[string]interface{})
-				data["access"] = token
-				data["mark"] = "TGV"
-				data["num"] = resultNum
-				data["order_no"] = GetRandomString(6)
-				bytesData, err := json.Marshal(data)
-				if err != nil {
-				    fmt.Println(err.Error() )
-				    return
-				}
-				reader := bytes.NewReader(bytesData)
-				url := "http://47.52.47.73:8112/auth/auth/subNum"
-				request, err := http.NewRequest("POST", url, reader)
-				if err != nil {
-				    fmt.Println(err.Error())
-				    return
-				}
-				request.Header.Set("Content-Type", "application/json;charset=UTF-8")
-				client := http.Client{}
-				resp, err := client.Do(request)
-				if err != nil {
-				    fmt.Println(err.Error())
-				    return
-				}
-				respBytes, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-				    fmt.Println(err.Error())
-				    return
-				}
-				//返回结果
-				var netReturn map[string]interface{}
-				json.Unmarshal(respBytes,&netReturn)
-				if netReturn["isSuccess"]==true{
-				    fmt.Printf("减币成功:\r\n%v",netReturn)
-				    /*****************减币成功********************/
-				    
-				    /************发送请求给聚合************/
-					data,err:=Post(juheURL,param)
-					if err!=nil{
-						fmt.Errorf("请求聚合失败,错误信息:\r\n%v",err)
-
-					}else{
-						var netReturn map[string]interface{}
-						json.Unmarshal(data,&netReturn)
-						fmt.Printf("请求聚合返回结果:\r\n%v",netReturn)
-					}
-					/***********发送请求给聚合************/
-
-				}else{
-					/*****************减币失败********************/
-				 
-				}
-				/***************减币成功才发送订单*****************/			    
-			/******************发送订单*****************/
 
 
 			t := time.Duration(dot_count)
